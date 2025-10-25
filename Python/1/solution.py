f8591978ee1a9ed606fe8093c1d45f718decc4f2
TomWatson6 <tom.watson6@outlook.com>
2025-10-25 15:54:36 +0100

    initial commit

---
+++ Python/1/solution.py
@@ -0,0 +1,36 @@
+
+input = open("input.txt").read().strip()
+input2 = open("input2.txt").read().strip()
+input3 = open("input3.txt").read().strip()
+
+def parse_input(input: str):
+    return ""
+
+def part1(input: str) -> int:
+    return 0
+
+def part2(input: str) -> int:
+    return 0
+
+def part3(input: str) -> int:
+    return 0
+
+if __name__ == "__main__":
+    print("Part 1:", part1(input))
+    print("Part 2:", part2(input2))
+    print("Part 3:", part3(input3))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Python/1/test_solution.py
@@ -0,0 +1,36 @@
+import unittest
+import solution
+
+input = open("simple_input.txt").read().strip()
+input2 = open("simple_input2.txt").read().strip()
+input3 = open("simple_input3.txt").read().strip()
+
+class Day01Test(unittest.TestCase):
+    def test_part1(self):
+        self.assertEqual(0, solution.part1(input))
+
+    def test_part2(self):
+        self.assertEqual(0, solution.part2(input2))
+
+    def test_part3(self):
+        self.assertEqual(0, solution.part3(input3))
+
+if __name__ == "__main__":
+    unittest.main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
